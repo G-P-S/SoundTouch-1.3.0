532926f20e1e6fed80337fe4d9714b38f9e2b141
Otto Sievert <osievert@gopro.com>
2013-03-12 13:49:26 -0700

    Fixed for x64 stdint.h address handing (CineForm RAW crash in CS6.) New installers for the improve AAC and MP4 intel (static linked to the IPP.)

--- source/TempoChange/TempoChange_uuid.c
+++ source/TempoChange/TempoChange_uuid.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Mar 05 18:01:05 2013
+/* at Mon Mar 11 22:35:03 2013
  */
 /* Compiler settings for TempoChange.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
